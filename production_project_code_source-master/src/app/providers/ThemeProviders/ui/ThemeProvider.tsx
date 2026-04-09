fae073cabaabbc919bc2d86b3bbf7c56308203f1
SanSan63kc <Reagent-163-RW-08@yandex.ru>
2026-04-09 17:39:21 +0400

    1/10

--- production_project_code_source-master/src/app/providers/ThemeProviders/ui/ThemeProvider.tsx
+++ production_project_code_source-master/src/app/providers/ThemeProviders/ui/ThemeProvider.tsx
@@ -1,5 +1,5 @@
 import { Children, FC, useMemo, useState } from "react"
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext"
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "../lib/ThemeContext"
 
 let defaultTheme =
   (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT
