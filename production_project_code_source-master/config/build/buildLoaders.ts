60c8208765ab4ae877186bb50501ae3576218406
SanSan63kc <Reagent-163-rw-08@yandex.ru>
2026-04-14 12:18:21 +0400

    2/17

--- production_project_code_source-master/config/build/buildLoaders.ts
+++ production_project_code_source-master/config/build/buildLoaders.ts
@@ -1,50 +1,68 @@
-import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import {BuildOptions} from "./types/config";
+import webpack from "webpack"
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import { BuildOptions } from "./types/config"
 
-export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  let svgLoader = {
+    test: /\.svg$/,
+    use: ["@svgr/webpack"],
+  }
 
-    let svgLoader = {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"]
-    }
+  let fileLoader = {
+    test: /\.(png|jpg?|gif|woff2|woff)$/i,
+    use: [
+      {
+        loader: "file-loader",
+      },
+    ],
+  }
 
-    let fileLoader = {
-        test: /\.(png|jpg?|gif|woff2|woff)$/i,
-        use: [
+  let babelLoader = {
+    test: /\.(js|jsx|ts|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: [
+          "@babel/preset-env",
+          "@babel/preset-typescript", // ОБЯЗАТЕЛЬНО
+          ["@babel/preset-react", { runtime: "automatic" }],
+        ],
+        plugins: [
+          [
+            "i18next-extract",
             {
-                loader: "file-loader",
-            }
-        ]
-    }
-
-    let cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: 'css-loader',
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
-                    },
-                }
+              locales: ["ru", "en"],
+              keyAsDefaultValue: true,
             },
-            'sass-loader',
-        ]
-    }
+          ],
+        ],
+      },
+    },
+  }
 
-    let typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+  let cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => Boolean(resPath.includes(".module.")),
+            localIdentName: isDev ? "[path][name]__[local]" : "[hash:base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  }
 
-    return [
-        fileLoader,
-        svgLoader,
-        typescriptLoader,
-        cssLoader,
-    ]
+  let typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  }
+
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader]
 }
