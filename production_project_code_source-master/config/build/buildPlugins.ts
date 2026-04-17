7328c7a97d82a6ad66c0a63c71c94df9c5209b7a
SanSan63kc <Reagent-163-rw-08@yandex.ru>
2026-04-17 08:57:29 +0400

    добавил пару переводов

--- production_project_code_source-master/config/build/buildPlugins.ts
+++ production_project_code_source-master/config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import path from "path";
 import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+//import {BundleAnalyzerPlugin} from "webpack-bundle-analyzer"
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
 
--- production_project_code_source-master/src/widgets/Navbar/ui/Navbar.tsx
+++ production_project_code_source-master/src/widgets/Navbar/ui/Navbar.tsx
@@ -2,25 +2,26 @@ import { classNames } from "shared/lib/classNames/classNames"
 import cls from "./Navbar.module.scss"
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink"
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher"
+import { useTranslation } from "react-i18next"
 
 interface NavbarProps {
   className?: string
 }
 
 export let Navbar = ({ className }: NavbarProps) => {
+  let { t } = useTranslation()
   return (
     <div className={classNames(cls.navbar, {}, [className || ""])}>
-      
       <div className={cls.links}>
         <AppLink
           theme={AppLinkTheme.SECONDARY}
           to={"/"}
           className={cls.mainLink}
         >
-          Главная
+          {t("Главная")}
         </AppLink>
         <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>
-          О сайте
+          {t("О сайте")}
         </AppLink>
       </div>
     </div>
