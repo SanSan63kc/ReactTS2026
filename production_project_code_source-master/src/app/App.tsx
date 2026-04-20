49721a719af58a2ad235be43af333fe43c2e352c
SanSan63kc <Reagent-163-rw-08@yandex.ru>
2026-04-20 15:39:18 +0400

    4/29 1/2

--- production_project_code_source-master/src/app/App.tsx
+++ production_project_code_source-master/src/app/App.tsx
@@ -4,15 +4,20 @@ import { classNames } from "shared/lib/classNames/classNames"
 import { AppRouter } from "./providers/router"
 import { Navbar } from "widgets/Navbar"
 import { Sidebar } from "widgets/Sidebar"
-import { Suspense } from "react"
+import { Suspense, useState } from "react"
+import { Modal } from "shared/ui/Modal/Modal"
 
 const App = () => {
   let { theme, toggleTheme } = useTheme()
+
+  let [isOpen, setIsOpen] = useState(false)
 
   return (
     <div className={classNames("app", {}, [theme])}>
       <Suspense fallback="">
         <Navbar />
+        <button onClick={()=>setIsOpen(true)}>toggle</button>
+        <Modal isOpen={isOpen} onClose={()=>setIsOpen(false)}/>
         <div className="content-page">
           <Sidebar />
           <AppRouter />
---
+++ production_project_code_source-master/src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,63 @@
+import { classNames } from "shared/lib/classNames/classNames"
+import cls from "./Modal.module.scss"
+import React, { ReactNode, useCallback, useEffect, useRef, useState } from "react"
+
+interface ModalProps {
+  className?: string
+  children?: ReactNode
+  isOpen?: boolean
+  onClose?: () => void
+}
+
+let ANIMATION_DELAY = 300
+
+export const Modal = (props: ModalProps) => {
+  let { className, children, isOpen, onClose } = props
+
+  let [isClosing, setIsClosing] = useState(false)
+  let timerRef = useRef<ReturnType<typeof setTimeout>>()
+
+  let closeHandler = useCallback(() => {
+    if (onClose) {
+      setIsClosing(true)
+      timerRef.current = setTimeout(() => {
+        onClose()
+        setIsClosing(false)
+      }, ANIMATION_DELAY)
+    }
+  }, [onClose])
+
+  let onKeyDown = useCallback((e:KeyboardEvent) =>{
+    if(e.key == "Escape"){
+        closeHandler()
+    }
+  }, [closeHandler])
+
+  let onContentClick = (e: React.MouseEvent) => {
+    e.stopPropagation()
+  }
+
+  useEffect(()=>{
+    if (isOpen){
+        window.addEventListener("keydown", onKeyDown)
+    }
+    return ()=>{
+        clearTimeout(timerRef.current)
+        window.removeEventListener("keydown", onKeyDown)
+    }
+  }, [isOpen, onKeyDown])
+
+  let mods: Record<string, boolean> = {
+    [cls.opened]: isOpen,
+    [cls.isClosing]: isClosing,
+  }
+  return (
+    <div className={classNames(cls.modal, mods, [className || ""])}>
+      <div className={cls.overlay} onClick={closeHandler}>
+        <div className={cls.content} onClick={onContentClick}>
+          Некий очень сильно большой текст{children}
+        </div>
+      </div>
+    </div>
+  )
+}
